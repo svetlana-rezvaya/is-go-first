a573f4bcfeb4242d864346f5f1e4e9fb7c84bf32
svetlana-rezvaya <svetlana-2305@mail.ru>
2022-03-10 22:19:34 +0300

    Add tests for the makeBasicAuthHeader function

---
+++ make_basic_auth_header_test.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+type env struct {
+	name     string
+	value    string
+	isNotSet bool
+}
+
+func updateEnvs(envs []env) error {
+	for _, env := range envs {
+		if env.isNotSet {
+			if err := os.Unsetenv(env.name); err != nil {
+				return fmt.Errorf("unable to unset an environment variable: %w", err)
+			}
+		} else {
+			if err := os.Setenv(env.name, env.value); err != nil {
+				return fmt.Errorf("unable to set an environment variable: %w", err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func Test_makeBasicAuthHeader(test *testing.T) {
+	type args struct {
+		usernameEnv env
+		passwordEnv env
+	}
+	type data struct {
+		name   string
+		args   args
+		wanted string
+	}
+
+	tests := []data{
+		data{
+			name: "without the username and the password",
+			args: args{
+				usernameEnv: env{name: "TEST_USERNAME", isNotSet: true},
+				passwordEnv: env{name: "TEST_PASSWORD", isNotSet: true},
+			},
+			wanted: "",
+		},
+		data{
+			name: "with the empty username and the empty password",
+			args: args{
+				usernameEnv: env{name: "TEST_USERNAME", value: ""},
+				passwordEnv: env{name: "TEST_PASSWORD", value: ""},
+			},
+			wanted: "",
+		},
+		data{
+			name: "with the nonempty username and the nonempty password",
+			args: args{
+				usernameEnv: env{name: "TEST_USERNAME", value: "username"},
+				passwordEnv: env{name: "TEST_PASSWORD", value: "password"},
+			},
+			wanted: "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+		},
+	}
+	for _, testData := range tests {
+		err := updateEnvs([]env{testData.args.usernameEnv, testData.args.passwordEnv})
+		if err != nil {
+			test.Logf("failed %q: %s", testData.name, err)
+			test.FailNow()
+		}
+		defer updateEnvs([]env{
+			env{name: testData.args.usernameEnv.name, isNotSet: true},
+			env{name: testData.args.passwordEnv.name, isNotSet: true},
+		})
+
+		received := makeBasicAuthHeader(
+			testData.args.usernameEnv.name,
+			testData.args.passwordEnv.name,
+		)
+
+		if received != testData.wanted {
+			test.Logf(
+				"failed %q:\n  expected: %+v\n  actual: %+v",
+				testData.name,
+				testData.wanted,
+				received,
+			)
+			test.Fail()
+		}
+	}
+}
