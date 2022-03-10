facacb9da9bff193c3518b8d54e962ee3fe5245e
svetlana-rezvaya <svetlana-2305@mail.ru>
2022-03-10 22:08:00 +0300

    Finish the makeBasicAuthHeader function

--- make_basic_auth_header.go
+++ make_basic_auth_header.go
@@ -1,10 +1,16 @@
 package main
 
-import "encoding/base64"
+import (
+	"encoding/base64"
+	"os"
+)
 
-func makeBasicAuthHeader() string {
-	username := ""
-	password := ""
+func makeBasicAuthHeader(usernameEnv string, passwordEnv string) string {
+	username := os.Getenv(usernameEnv)
+	password := os.Getenv(passwordEnv)
+	if username == "" || password == "" {
+		return ""
+	}
 
 	credentials := username + ":" + password
 	credentials = base64.StdEncoding.EncodeToString([]byte(credentials))
